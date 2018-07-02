1aa50059d2f2b84865cc10c80568490fdc48066d
NiYiHao <s10024603@gmail.com>
2018-07-02 16:59:55 +0800

    Project_ChatroomLab0.0.5

--- app/src/main/java/com/example/administrator/my_project_chatroomlab/RoomActivity.java
+++ app/src/main/java/com/example/administrator/my_project_chatroomlab/RoomActivity.java
@@ -2,6 +2,9 @@ package com.example.administrator.my_project_chatroomlab;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
 
 public class RoomActivity extends AppCompatActivity {
 
@@ -9,5 +12,17 @@ public class RoomActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate( savedInstanceState );
         setContentView( R.layout.activity_room );
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        MenuInflater menuInflater = new MenuInflater( this );
+        menuInflater.inflate( R.menu.add , menu );
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        return super.onOptionsItemSelected( item );
     }
 }

0fafb9e04872954868d039465c1ae053aaf1e163
NiYiHao <s10024603@gmail.com>
2018-07-02 13:57:17 +0800

    Project_ChatroomLab0.0.4

--- app/src/main/java/com/example/administrator/my_project_chatroomlab/ChatRoomActivity.java
+++ app/src/main/java/com/example/administrator/my_project_chatroomlab/ChatRoomActivity.java
@@ -3,6 +3,7 @@ package com.example.administrator.my_project_chatroomlab;
 import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 import android.view.animation.Animation;
 import android.view.animation.AnimationUtils;
 import android.widget.LinearLayout;
@@ -12,6 +13,10 @@ public class ChatRoomActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate( savedInstanceState );
+
+        getSupportActionBar().hide(); //隱藏標題
+        getWindow().getDecorView().setSystemUiVisibility( View.SYSTEM_UI_FLAG_FULLSCREEN); //隱藏狀態
+
         setContentView( R.layout.activity_chat_room );
         LinearLayout logo =findViewById( R.id.logo );
         Animation fadein = AnimationUtils.loadAnimation( this, R.anim.fadein );
--- app/src/main/java/com/example/administrator/my_project_chatroomlab/LoginActivity.java
+++ app/src/main/java/com/example/administrator/my_project_chatroomlab/LoginActivity.java
@@ -1,13 +1,29 @@
 package com.example.administrator.my_project_chatroomlab;
 
+import android.annotation.SuppressLint;
+import android.content.Intent;
+import android.provider.ContactsContract;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ImageButton;
 
 public class LoginActivity extends AppCompatActivity {
-
+    Button btnlogin;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate( savedInstanceState );
         setContentView( R.layout.activity_login );
+        btnlogin = findViewById( R.id.btnlogin );
+        btnlogin.setOnClickListener( new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent i = new Intent();
+                i.setClass( LoginActivity.this , RoomActivity.class );
+                startActivity( i );
+            }
+        } );
+
     }
 }
